a68e8cec15d77b6c6a97658b6093ef5256a4e9d8
vegiwoo <vegiwoo@protonmail.com>
2020-08-04 14:47:10 +0300

    Add NSRegularExpression extensions

---
+++ Sources/SwiftHelperCode/Extensions/NSRegularExpression + Ext.swift
@@ -0,0 +1,25 @@
+
+//  NSRegularExpression + Ext.swift
+//  Created by Dmitry Samartcev on 04.08.2020.
+
+import Foundation
+
+public extension NSRegularExpression {
+    /// Initializes a regular expression from a string pattern
+    /// - Parameter pattern: String pattern for the regular expression.
+    convenience init(_ pattern: String) {
+        do {
+            try self.init(pattern: pattern)
+        } catch {
+            preconditionFailure("Invalid regex: \(pattern).")
+        }
+    }
+    
+    /// Checks a string against a regular expression.
+    /// - Parameter string: Input string to check.
+    /// - Returns:  Result of check as a boolean flag.
+    func matches(_ string: String) -> Bool {
+        let range = NSRange(location: 0, length: string.utf16.count)
+        return firstMatch(in: string, options: [], range: range) != nil
+    }
+}
