d6e24262909b17c754f6d6dd6aed2bf934c54578
vegiwoo <vegiwoo@protonmail.com>
2020-09-16 11:26:36 +0300

    Add UIImage extensions for IOS

--- Sources/SwiftHelperCode/Models/JWT/MicroservicesPayloadCodingKeys01.swift
+++ Sources/SwiftHelperCode/Models/JWT/MicroservicesPayloadCodingKeys01.swift
@@ -1,8 +1,9 @@
-//
-//  File.swift
-//  
-//
+//  MicroservicesPayloadCodingKeys01.swift
 //  Created by Dmitry Samartcev on 14.09.2020.
-//
 
 import Foundation
+
+public enum MicroservicesPayloadCodingKeys01: String, CodingKey {
+    case subject        = "sub"
+    case expiration     = "exp"
+}
--- Sources/SwiftHelperCode/Models/JWT/UsersPayloadCodingKeys01.swift
+++ Sources/SwiftHelperCode/Models/JWT/UsersPayloadCodingKeys01.swift
@@ -1,4 +1,4 @@
-//  UsersPayloadCodingKeys.swift
+//  UsersPayloadCodingKeys01.swift
 //  Created by Dmitry Samartcev on 14.09.2020.
 
 import Foundation
@@ -12,7 +12,4 @@ public enum UsersPayloadCodingKeys01: String, CodingKey {
     case userStatus     = "userStatus"
 }
 
-public enum MicroservicesPayloadCodingKeys01: String, CodingKey {
-    case subject        = "sub"
-    case expiration     = "exp"
-}
+
