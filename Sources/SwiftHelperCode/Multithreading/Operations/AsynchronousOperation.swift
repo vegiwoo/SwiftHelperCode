df3e2f344c5402b128bad256430df462a05a9879
vegiwoo <vegiwoo@protonmail.com>
2020-11-20 22:18:17 +0300

    Multithreading - AsynchronousOperation, ResultDrivenOperation

---
+++ Sources/SwiftHelperCode/Multithreading/Operations/AsynchronousOperation.swift
@@ -0,0 +1,57 @@
+//  AsynchronousOperation.swift
+//  Created by Dmitry Samartcev on 20.11.2020.
+
+#if canImport(UIKit)
+import Foundation
+
+/// Represents entity of an asynchronous operation
+///
+/// Allows running long running tasks without having to block the calling thread until execution is complete
+public class AsynchronousOperation : Operation {
+    
+    private let lockQueue  = DispatchQueue(label: "tech.vegiwoo.asyncOperationLockQueue")
+    
+    public override var isAsynchronous: Bool {
+        return true
+    }
+    
+    private var _isExecuting: Bool = false
+    public override var isExecuting: Bool {
+        get { lockQueue.sync { _isExecuting } }
+        set {
+            willChangeValue(forKey: "isExecuting")
+            lockQueue.sync { _isExecuting = newValue }
+            didChangeValue(forKey: "isExecuting")
+        }
+    }
+    
+    private var _isFinished: Bool = false
+    public override var isFinished: Bool {
+        get { lockQueue.sync { _isFinished } }
+        set {
+            willChangeValue(forKey: "isFinished")
+            lockQueue.sync { _isFinished = newValue }
+            didChangeValue(forKey: "isFinished")
+        }
+    }
+    
+    public override func start() {
+        self.isFinished  = false
+        self.isExecuting = true
+        main()
+    }
+    
+    public override func main() {
+        guard !isCancelled else { return }
+        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
+            print("asyncOperation perform")
+            self.finish()
+        }
+    }
+     
+    func finish() {
+        self.isExecuting = false
+        self.isFinished = true
+    }
+}
+#endif
---
+++ Sources/SwiftHelperCode/Multithreading/Operations/FileDownloadOperation.swift
@@ -0,0 +1,36 @@
+//  FileDownloadOperation.swift
+//  Created by Dmitry Samartcev on 20.11.2020.
+
+import Foundation
+
+#if canImport(UIKit)
+/// Asynchronous result-oriented operation
+public class ResultDrivenOperation<Success, Failure> : AsynchronousOperation where Failure: Error {
+    private(set) var result : Result<Success,Failure>? {
+        didSet {
+            guard let result = result else { return }
+            onResult?(result)
+        }
+    }
+    
+    public var onResult: ((_ result:  Result<Success,Failure>) -> Void)?
+    
+    public override func finish() {
+        fatalError("You should use finish(with:) to ensure a result.")
+    }
+    
+    public func finish(with result: Result<Success,Failure>) {
+        self.result = result
+        super.finish()
+    }
+    
+    public override func cancel() {
+        fatalError("You should use cancel(with:) to ensure a result.")
+    }
+    
+    public func cancel(with error: Failure) {
+        result = .failure(error)
+        super.cancel()
+    }
+}
+#endif
