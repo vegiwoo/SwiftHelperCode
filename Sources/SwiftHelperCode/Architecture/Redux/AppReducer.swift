9c5cd9021f238976e5f1e0a5288592cb042abda3
vegiwoo <vegiwoo@protonmail.com>
2021-04-23 13:56:45 +0300

    Changing access levels 03

--- Sources/SwiftHelperCode/Architecture/Redux/AppReducer.swift
+++ Sources/SwiftHelperCode/Architecture/Redux/AppReducer.swift
@@ -1,8 +1,28 @@
 //
-//  File.swift
-//  
-//
+//  AppReducer.swift
 //  Created by Dmitry Samartcev on 23.04.2021.
 //
 
 import Foundation
+import Combine
+
+public struct AppReducer<State, Action, Environment> {
+
+    public let reduce: (inout State, Action, Environment) -> AnyPublisher<Action, Never>
+    
+    public func callAsFunction(_ state: inout State, _ action: Action, _ environment: Environment) -> AnyPublisher<Action, Never> {
+        reduce(&state, action, environment)
+    }
+    
+    public func optional() -> AppReducer<State?, Action, Environment> {
+        .init() {state, action, environment in
+            if state != nil {
+                return self(&state!, action, environment)
+            } else {
+                return Empty(completeImmediately: true).eraseToAnyPublisher()
+            }
+        }
+    }
+}
+
+
