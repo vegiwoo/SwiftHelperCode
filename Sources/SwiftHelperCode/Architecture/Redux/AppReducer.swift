54f23dffcfac26abe23100bc20712b1f997d2789
vegiwoo <vegiwoo@protonmail.com>
2021-04-23 13:02:48 +0300

    Changing access levels

--- Sources/SwiftHelperCode/Architecture/Redux/AppReducer.swift
+++ Sources/SwiftHelperCode/Architecture/Redux/AppReducer.swift
@@ -6,13 +6,13 @@ import SwiftUI
 import Combine
 
 public struct AppReducer<State, Action, Environment> {
-    let reduce: (inout State, Action, Environment) -> AnyPublisher<Action, Never>
+    public let reduce: (inout State, Action, Environment) -> AnyPublisher<Action, Never>
     
-    func callAsFunction(_ state: inout State, _ action: Action, _ environment: Environment) -> AnyPublisher<Action, Never> {
+    public func callAsFunction(_ state: inout State, _ action: Action, _ environment: Environment) -> AnyPublisher<Action, Never> {
         reduce(&state, action, environment)
     }
     
-    func optional() -> AppReducer<State?, Action, Environment> {
+    public func optional() -> AppReducer<State?, Action, Environment> {
         .init {state, action, environment in
             if state != nil {
                 return self(&state!, action, environment)
