9306be89fccd266f7847fc45c912bdfc5ab31829
vegiwoo <vegiwoo@protonmail.com>
2021-05-17 16:49:24 +0400

    Add ViewControllerResolver & ParentResolverViewController for work with SwiftUI

---
+++ Sources/SwiftHelperCode/Helpers/ViewControllerResolver.swift
@@ -0,0 +1,27 @@
+//  ViewControllerResolver.swift
+//  Created by Dmitry Samartcev on 17.05.2021.
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+/// Implements interaction with UIViewController from SwiftUI.
+public final class ViewControllerResolver: UIViewControllerRepresentable {
+    
+    public func makeUIViewController(context: Context) -> ParentResolverViewController {
+        ParentResolverViewController()
+    }
+    
+    public func updateUIViewController(_ uiViewController: ParentResolverViewController, context: Context) { }
+}
+
+public class ParentResolverViewController: UIViewController {
+    
+    public override func didMove(toParent parent: UIViewController?) {
+        super.didMove(toParent: parent)
+        
+        if let parent = parent {
+            print("didMove(toParent: \(parent)")
+        }
+    }
+}
+#endif
