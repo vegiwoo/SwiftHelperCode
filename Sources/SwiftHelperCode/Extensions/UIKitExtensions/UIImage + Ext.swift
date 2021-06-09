140f86bbd3e942f0f007bc61ce3afe6e8135a475
vegiwoo <vegiwoo@protonmail.com>
2021-06-09 12:02:30 +0300

    Add isLight() extension fot UIImage

--- Sources/SwiftHelperCode/Extensions/UIKitExtensions/UIImage + Ext.swift
+++ Sources/SwiftHelperCode/Extensions/UIKitExtensions/UIImage + Ext.swift
@@ -33,6 +33,7 @@ public extension UIImage {
         return newImage
     }
     
+    /// Specifies average color in image.
     var averageColor: UIColor? {
         guard let inputImage = CIImage(image: self) else { return nil }
         let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
@@ -45,6 +46,19 @@ public extension UIImage {
         context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: .RGBA8, colorSpace: nil)
 
         return UIColor(red: CGFloat(bitmap[0]) / 255, green: CGFloat(bitmap[1]) / 255, blue: CGFloat(bitmap[2]) / 255, alpha: CGFloat(bitmap[3]) / 255)
+    }
+    
+    /// Determines the predominance of a dark or light color in image.
+    /// - Returns: Bool flag.
+    /// - Remark: Algorithm from: http://www.w3.org/WAI/ER/WD-AERT/#color-contrast
+    static func isLight() -> Bool {
+        guard let components = self.cgColor.components else { return false }
+        let components01 = (components[0] * 299)
+        let components02 = (components[1] * 587)
+        let components03 = (components[2] * 114)
+        
+        let brightness = ( components01 + components02 + components03) / 1000
+        return brightness < 0.5 ? false : true
     }
 }
 #endif
