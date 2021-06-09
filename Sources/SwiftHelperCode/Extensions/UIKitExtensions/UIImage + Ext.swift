4cbace1aea1688182c85aed58b9647f2515a7821
vegiwoo <vegiwoo@protonmail.com>
2021-06-09 11:30:59 +0300

    Add average Color extension for UIImage

--- Sources/SwiftHelperCode/Extensions/UIKitExtensions/UIImage + Ext.swift
+++ Sources/SwiftHelperCode/Extensions/UIKitExtensions/UIImage + Ext.swift
@@ -33,7 +33,7 @@ public extension UIImage {
         return newImage
     }
     
-    static var averageColor: UIColor? {
+    var averageColor: UIColor? {
         guard let inputImage = CIImage(image: self) else { return nil }
         let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
 
