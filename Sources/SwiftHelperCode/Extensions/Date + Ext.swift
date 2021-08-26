f6ba7736068acab9c02e1d700844774148e09f5b
vegiwoo <vegiwoo@protonmail.com>
2021-08-26 19:45:58 +0300

    Add extenstion for Date (work with millisecondsSince1970)

--- Sources/SwiftHelperCode/Extensions/Date + Ext.swift
+++ Sources/SwiftHelperCode/Extensions/Date + Ext.swift
@@ -72,6 +72,19 @@ public extension Date {
         return Calendar.current.date(byAdding: component, value: number, to: originalDate)
     }
     
+    /// Represents a date in format timeIntervalSince1970.
+    ///
+    /// Check correctness: https://www.epochconverter.com/
+    var millisecondsSince1970: Int64 {
+        return Int64((self.timeIntervalSince1970 * 1000.0).rounded())
+    }
+
+    /// Initializes date from timeIntervalSince1970.
+    /// - Parameter milliseconds: timeIntervalSince1970 value.
+    init(milliseconds: Int64) {
+        self = Date(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
+    }
+    
 }
 
 public enum DateTimeFormats : Int, CaseIterable {
