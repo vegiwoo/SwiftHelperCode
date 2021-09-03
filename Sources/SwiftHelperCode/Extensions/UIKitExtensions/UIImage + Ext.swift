cf0d41c866d62a32e9b8543c178191fd55448e7b
vegiwoo <vegiwoo@protonmail.com>
2021-09-03 18:12:58 +0300

    Adding a function to compare two UIImages

--- Sources/SwiftHelperCode/Extensions/UIKitExtensions/UIImage + Ext.swift
+++ Sources/SwiftHelperCode/Extensions/UIKitExtensions/UIImage + Ext.swift
@@ -7,12 +7,17 @@ import UIKit
 
 public extension UIImage {
     
+    /// Resizes the image.
+    /// - Parameters:
+    ///     - image: Original image.
+    ///     - targetSize: Specified size for a new measurement.
+    /// - Returns: Modified image.
     static func resizeImage(_ image: UIImage, targetSize: CGSize) -> UIImage? {
         let size = image.size
-
+        
         let widthRatio  = targetSize.width  / image.size.width
         let heightRatio = targetSize.height / image.size.height
-
+        
         // Figure out what our orientation is, and use that to form the rectangle
         var newSize: CGSize
         if(widthRatio > heightRatio) {
@@ -20,16 +25,16 @@ public extension UIImage {
         } else {
             newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
         }
-
+        
         // This is the rect that we've calculated out and this is what is actually used below
         let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
-
+        
         // Actually do the resizing to the rect using the ImageContext stuff
         UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
         image.draw(in: rect)
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-
+        
         return newImage
     }
     
@@ -37,15 +42,29 @@ public extension UIImage {
     var averageColor: UIColor? {
         guard let inputImage = CIImage(image: self) else { return nil }
         let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
-
+        
         guard let filter = CIFilter(name: "CIAreaAverage", parameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: extentVector]) else { return nil }
         guard let outputImage = filter.outputImage else { return nil }
-
+        
         var bitmap = [UInt8](repeating: 0, count: 4)
         let context = CIContext(options: [.workingColorSpace: kCFNull!])
         context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: .RGBA8, colorSpace: nil)
-
+        
         return UIColor(red: CGFloat(bitmap[0]) / 255, green: CGFloat(bitmap[1]) / 255, blue: CGFloat(bitmap[2]) / 255, alpha: CGFloat(bitmap[3]) / 255)
     }
+    
+    
+    /// Compares two images.
+    /// - Parameter image: Image for comparison.
+    /// - Returns: Boolean flag for performing the operation.
+    public func isEqualToImage(image: UIImage) -> Bool {
+        if let selfData = self.pngData() as NSData?,
+           let otherData = image.pngData() as NSData? {
+            return selfData.isEqual(otherData)
+        } else {
+            return false
+        }
+    }
+
 }
 #endif
