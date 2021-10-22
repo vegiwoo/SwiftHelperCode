a1e6a590318d55faae0bbf1f2d3f85556db79b48
vegiwoo <vegiwoo@protonmail.com>
2021-10-22 17:57:59 +0300

    Add ARViewWrapperCoordinator, ARViewWrapperCoordinatorDelegate

--- Sources/SwiftHelperCode/ARHelpers/Views/ARViewWrapper.swift
+++ Sources/SwiftHelperCode/ARHelpers/Views/ARViewWrapper.swift
@@ -8,23 +8,66 @@ import RealityKit
 
 public struct ARViewWrapper: UIViewRepresentable {
     
-    @Binding var arSession: ARSession
-    @Binding var debugOptions: ARView.DebugOptions
+    @Binding public var arSession: ARSession
+    @Binding public var debugOptions: ARView.DebugOptions
+    private var coordinatorDelegate: ARViewWrapperCoordinatorDelegate
     
-    public init (arSession: Binding<ARSession>, debugOptions: Binding<ARView.DebugOptions>) {
+    @State public var isTapGesture: Bool = false
+    
+    public init (arSession: Binding<ARSession>, debugOptions: Binding<ARView.DebugOptions>,
+                 delegate: ARViewWrapperCoordinatorDelegate) {
         self._arSession = arSession
         self._debugOptions = debugOptions
+        self.coordinatorDelegate = delegate
     }
     
+    public func makeCoordinator() -> ARViewWrapperCoordinator {
+        ARViewWrapperCoordinator()
+    }
+
     public func makeUIView(context: Context) -> ARView {
+        // Create ARView.
         let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: false)
         arView.session = arSession
         arView.debugOptions = debugOptions
+        // Set the required values for the coordinator:
+        context.coordinator.parent = arView
+        context.coordinator.delegate = coordinatorDelegate
         return arView
     }
     
     public func updateUIView(_ uiView: ARView, context: Context) {
-        // Do something...
+        if isTapGesture, !context.coordinator.isTapGesture {
+            context.coordinator.isTapGesture.toggle()
+        }
     }
+
 }
 #endif
+
+public final class ARViewWrapperCoordinator: NSObject {
+    
+    public var parent: ARView?
+    weak var delegate: ARViewWrapperCoordinatorDelegate?
+    
+    public var isTapGesture: Bool = false {
+        didSet {
+            if isTapGesture {
+                enableTapGesture(with: #selector(delegate?.tapHandler(sender:)))
+            }
+        }
+    }
+    
+    public override init() {
+        super.init()
+    }
+    
+    func enableTapGesture(with handler: Selector?) {
+        let tapGestureRecognizer: UITapGestureRecognizer = .init(target: parent, action: handler)
+        parent?.addGestureRecognizer(tapGestureRecognizer)
+    }
+}
+
+@objc public protocol ARViewWrapperCoordinatorDelegate {
+    @objc func tapHandler(sender: UITapGestureRecognizer) -> Void
+}
